1b2e260787b5a88ed080959cce14a0c5d759dd5c
Dimitri van Velde <9016367@student.zadkine.nl>
2021-12-08 16:13:37 +0100

    contactform update

--- contactform.php
+++ contactform.php
@@ -25,7 +25,18 @@
                 <?php 
                     if(isset($_SESSION["userid"])) {
                 ?>
-                <form class="contact" action="contactform.php" method="post">
+                <form class="contact" action="php-includes/contact.inc.php" method="post">
+                    <?php
+                        if(isset($_GET["error"])) {
+                            if($_GET["error"] == "none") {
+                    ?>
+                        <span class="login-check-message"><img src="images/check.svg" alt="Check Icon">
+                            <p>Het formulier is succesvol verzonden. We hopen zo spoedig mogelijk te antwoorden.</p>
+                        </span>
+                    <?php
+                            }
+                        }
+                    ?>
                     <h2>Vragen?</h2>
                     <h3>We helpen U graag!</h3>
                     <fieldset>
@@ -99,7 +110,7 @@
                 <?php
                     } else {
                 ?>
-                <form class="contact" action="contactform.php" method="post">
+                <form class="contact" action="php-includes/contact.inc.php" method="post">
                     <h2>Vragen?</h2>
                     <h3>We helpen U graag!</h3>
                     <fieldset>
@@ -165,270 +176,6 @@
                 </form>
                 <?php
                     }
-                ?>
-
-
-                <?php
-                
-                    // Variables
-                    // Form Info
-                    $vNaam = $_POST["voornaam"] ?? "";
-                    $aNaam = $_POST["achternaam"] ?? "";
-                    $email = $_POST["email"] ?? "";
-                    $tel = $_POST["tel"] ?? "";
-                    $rating = $_POST["rating"] ?? "";
-                    $vraag = $_POST["vraag"] ?? "";
-                    $mening = $_POST["mening"] ?? "";
-                    $bug = $_POST["bug"] ?? "";
-                    $vraagSoort = $_POST["soort"] ?? "";
-                    $bugPage = $_POST["bug_page"] ?? "";
-
-                    // Timestamp
-                    $timestamp = date("c");
-                    
-                    // Webhook URL
-                    $url = "https://discord.com/api/webhooks/910252315573895218/VJPppRzEJOj7dI3ZRcHis-THXvOqodo7dc0ouEUDB6xV8womwvzxqOEFY4Bv-Kz6ztsM";
-
-                    // Embed for Discord
-
-                    // Embed for Question
-                    if($vraagSoort == "vraag") {
-                        $embedResult = json_encode([
-                            "username" => "Contactformulier",
-                            "content" => "Er is een vraag formulier ingevult.",
-                            "avatar_url" => "https://gamednm.com/logo.png",
-                            "tts" => false,
-                            "embeds" => [
-                                [
-                                    "title" => "Contact formulier van $vNaam $aNaam",
-                                    "type" => "rich",
-                                    // Make sure color is in decimal!!
-                                    "color" => "1776411",
-                                    // Make sure timestamp is formatted as ISO8601
-                                    "timestamp" => $timestamp,
-                                    "footer" => [
-                                        "text" => "www.quokkamobile.nl",
-                                        "icon_url" => "https://gamednm.com/logo.png"
-                                    ],
-                                    "thumbnail" =>  [
-                                        "url" => "https://gamednm.com/logo.png"
-                                    ],
-                                    "fields" => [
-                                        [
-                                            "name" => "Voornaam: ",
-                                            "value" => $vNaam
-                                        ],
-                                        [
-                                            "name" => "Achternaam: ",
-                                            "value" => $aNaam
-                                        ],
-                                        [
-                                            "name" => "Emailadres: ",
-                                            "value" => $email
-                                        ],
-                                        [
-                                            "name" => "Telefoonnummer: ",
-                                            "value" => $tel
-                                        ],
-                                        [
-                                            "name" => "Rating: ",
-                                            "value" => $rating . " sterren"
-                                        ],
-                                        [
-                                            "name" => "Vraag: ",
-                                            "value" => $vraag
-                                        ]
-                                    ]
-                                ]
-
-                            ]
-                        ], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE );
-
-                        // Check if form is filled in
-                        if($vNaam != "" && $aNaam != "" && $email != "" && $tel != "" && $vraag != "" && $rating != "") {
-
-                            // Alert function
-                            function alert($alertMsg) {
-                                echo "<script type='text/javascript'>alert('$alertMsg');</script>";
-                            }
-                            
-                            // Send to Discord
-                            $ch = curl_init();
-
-                            curl_setopt_array( $ch, [
-                                CURLOPT_URL => $url,
-                                CURLOPT_POST => true,
-                                CURLOPT_POSTFIELDS => $embedResult,
-                                CURLOPT_HTTPHEADER => [
-                                    "Length" => strlen($embedResult),
-                                    "Content-Type: application/json"
-                                ]
-                            ]);
-
-                            $response = curl_exec( $ch );
-                            curl_close( $ch );
-                            alert("Bedankt voor het vragen $vNaam, we hopen u zo snel mogelijk te kunnen helpen!");
-                        }
-                    } else if($vraagSoort == "mening") {
-                        $embedResult = json_encode([
-                            "username" => "Contactformulier",
-                            "content" => "Er is een menings formulier ingevult.",
-                            "avatar_url" => "https://gamednm.com/logo.png",
-                            "tts" => false,
-                            "embeds" => [
-                                [
-                                    "title" => "Contact formulier van $vNaam $aNaam",
-                                    "type" => "rich",
-                                    // Make sure color is in decimal!!
-                                    "color" => "1776411",
-                                    // Make sure timestamp is formatted as ISO8601
-                                    "timestamp" => $timestamp,
-                                    "footer" => [
-                                        "text" => "www.quokkamobile.nl",
-                                        "icon_url" => "https://gamednm.com/logo.png"
-                                    ],
-                                    "thumbnail" =>  [
-                                        "url" => "https://gamednm.com/logo.png"
-                                    ],
-                                    "fields" => [
-                                        [
-                                            "name" => "Voornaam: ",
-                                            "value" => $vNaam
-                                        ],
-                                        [
-                                            "name" => "Achternaam: ",
-                                            "value" => $aNaam
-                                        ],
-                                        [
-                                            "name" => "Emailadres: ",
-                                            "value" => $email
-                                        ],
-                                        [
-                                            "name" => "Telefoonnummer: ",
-                                            "value" => $tel
-                                        ],
-                                        [
-                                            "name" => "Rating: ",
-                                            "value" => $rating . " sterren"
-                                        ],
-                                        [
-                                            "name" => "Mening: ",
-                                            "value" => $mening
-                                        ]
-                                    ]
-                                ]
-
-                            ]
-                        ], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE );
-
-                        // Check if form is filled in
-                        if($vNaam != "" && $aNaam != "" && $email != "" && $tel != "" && $mening != "" && $rating != "") {
-
-                            // Alert function
-                            function alert($alertMsg) {
-                                echo "<script type='text/javascript'>alert('$alertMsg');</script>";
-                            }
-                            
-                            // Send to Discord
-                            $ch = curl_init();
-
-                            curl_setopt_array( $ch, [
-                                CURLOPT_URL => $url,
-                                CURLOPT_POST => true,
-                                CURLOPT_POSTFIELDS => $embedResult,
-                                CURLOPT_HTTPHEADER => [
-                                    "Length" => strlen($embedResult),
-                                    "Content-Type: application/json"
-                                ]
-                            ]);
-
-                            $response = curl_exec( $ch );
-                            curl_close( $ch );
-                            alert("Bedankt voor het geven van uw mening $vNaam!");
-                        }
-                    } else if($vraagSoort == "bug") {
-                        $embedResult = json_encode([
-                            "username" => "Contactformulier",
-                            "content" => "Er is een bug formulier ingevult.",
-                            "avatar_url" => "https://gamednm.com/logo.png",
-                            "tts" => false,
-                            "embeds" => [
-                                [
-                                    "title" => "Contact formulier van $vNaam $aNaam",
-                                    "type" => "rich",
-                                    // Make sure color is in decimal!!
-                                    "color" => "1776411",
-                                    // Make sure timestamp is formatted as ISO8601
-                                    "timestamp" => $timestamp,
-                                    "footer" => [
-                                        "text" => "www.quokkamobile.nl",
-                                        "icon_url" => "https://gamednm.com/logo.png"
-                                    ],
-                                    "thumbnail" =>  [
-                                        "url" => "https://gamednm.com/logo.png"
-                                    ],
-                                    "fields" => [
-                                        [
-                                            "name" => "Voornaam: ",
-                                            "value" => $vNaam
-                                        ],
-                                        [
-                                            "name" => "Achternaam: ",
-                                            "value" => $aNaam
-                                        ],
-                                        [
-                                            "name" => "Emailadres: ",
-                                            "value" => $email
-                                        ],
-                                        [
-                                            "name" => "Telefoonnummer: ",
-                                            "value" => $tel
-                                        ],
-                                        [
-                                            "name" => "Rating: ",
-                                            "value" => $rating . " sterren"
-                                        ],
-                                        [
-                                            "name" => "Locatie Bug: ",
-                                            "value" => $bugPage
-                                        ],
-                                        [
-                                            "name" => "Bug: ",
-                                            "value" => $bug
-                                        ]
-                                    ]
-                                ]
-
-                            ]
-                        ], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE );
-
-                        // Check if form is filled in
-                        if($vNaam != "" && $aNaam != "" && $email != "" && $tel != "" && $bug != "" && $rating != "") {
-
-                            // Alert function
-                            function alert($alertMsg) {
-                                echo "<script type='text/javascript'>alert('$alertMsg');</script>";
-                            }
-                            
-                            // Send to Discord
-                            $ch = curl_init();
-
-                            curl_setopt_array( $ch, [
-                                CURLOPT_URL => $url,
-                                CURLOPT_POST => true,
-                                CURLOPT_POSTFIELDS => $embedResult,
-                                CURLOPT_HTTPHEADER => [
-                                    "Length" => strlen($embedResult),
-                                    "Content-Type: application/json"
-                                ]
-                            ]);
-
-                            $response = curl_exec( $ch );
-                            curl_close( $ch );
-                            alert("Bedankt voor het aangeven van de fout $vNaam, we hopen het zo snel mogelijk op te lossen!");
-                        }
-                    }
-
                 ?>
 
                 <script>
